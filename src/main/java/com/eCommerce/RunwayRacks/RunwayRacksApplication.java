d1736a9f57fe9855c430db2cd749d585cece7ab0
saiswaroop5225 <sjswaroop5225@gmail.com>
2024-08-30 22:15:42 +0530

    Structure(Dependencies, Artifacts, Configurations): Spring initializer with version of java 21, spring6 3.3.3 and Project Gradle-Groovy

---
+++ src/main/java/com/eCommerce/RunwayRacks/RunwayRacksApplication.java
@@ -0,0 +1,13 @@
+package com.eCommerce.RunwayRacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunwayRacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunwayRacksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eCommerce/RunwayRacks/RunwayRacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eCommerce.RunwayRacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunwayRacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
